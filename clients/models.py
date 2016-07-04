8ac47c7847c6a76c91b6dd5b6b474d71fadc448a
Irving Kcam <me@ikcam.com>
2016-07-04 11:27:16 -0500

    Update models.py

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Client(models.Model):
 	phone = models.IntegerField(unique=True)
 	email = models.EmailField(max_length=255)
 	address = models.CharField(max_length=255)
+	user = models.ForeignKey(User)
 
 	def __str__(self):
 		return self.name
